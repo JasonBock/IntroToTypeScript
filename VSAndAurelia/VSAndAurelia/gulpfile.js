2a910c3f48fef5ae5907a41c8ee40c94ee60f930
Jason Bock <jason@jasonbock.net>
2017-03-13 23:37:07 -0700

    Update for TS and app example

--- VSAndAurelia/VSAndAurelia/gulpfile.js
+++
@@ -1,4 +0,0 @@
-﻿var gulp = require('gulp'); gulp.task('copy-typings', function () {
-	return gulp.src('jspm_packages/**/*.d.ts')
-	.pipe(gulp.dest('scripts/typings/jspm_packages/'));
-});
--- VSAndAurelia/VSAndAurelia/src/app.js
+++
@@ -1,8 +0,0 @@
-var App = (function () {
-    function App() {
-        this.name = "Aurelia SHOW THE CHANGE";
-    }
-    return App;
-})();
-exports.App = App;
-//# sourceMappingURL=app.js.map
